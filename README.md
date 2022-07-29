928fb7cbdfb2bd0e66dfd8cd81060eff40cacf4e
samldr <sleader@ualberta.ca>
2022-07-29 16:11:20 -0600

    fixed space issues

--- README.md
+++ README.md
@@ -3,10 +3,8 @@ I wrote this translator to turn TeX into a format that calculators can read and 
 
 ## Current Limitations:
 
-    - no recursive fractions
-    - no recursive sqrts
-    - backslash commands must be separated by a space (ex. '(x+3) \cdot (x+4)) in the input
-    - exponents must also be separated by a space  (ex. 'x ^{3x+2} *23) in the input
+    - no recursive fractions or sqrts in fractions
+
 
 ## To-do:
     - fix all the limitations
--- tex2cas.js
+++ tex2cas.js
@@ -1,12 +1,65 @@
 // example equation
-const inputString =  String.raw`x \cdot \left( \frac{\sqrt{2x+4}}{3+2} \right) ^{3x+2} 2x = 12x`;
+const inputString =  String.raw`\frac{3\cdot 5}{4x}\times \sqrt{2x+45}+4^{ex+2e}`;
 
 function translate(inputString){
-    const inputArr = inputString.split(' ');
+    const spaceArr = inputString.split('');
     const accSym = ['+', '-', '=', '(', ')', '*'];
 
     let inputQueue = [];
+    let inputArr = [];
+    let output = [];
+    
+    let bracketStack = [];
+    let slashStack = [];
 
+    // add spaces
+    for (let i = 0; i < spaceArr.length; i++){
+        if (spaceArr[i] === '{'){
+            bracketStack.push('{');
+        } else if(spaceArr[i] === '}'){
+            bracketStack.pop()
+        }
+
+        if (spaceArr[i] === '\\'){
+            slashStack.push('/')
+        }
+
+        if (spaceArr[i] === ' '){
+            spaceArr[i] = '~';
+        }
+        
+        if((spaceArr[i] === '\\' ||
+        spaceArr[i] === '^') &&
+        bracketStack.length === 0){
+
+            inputArr.push(' ')
+            inputArr.push(spaceArr[i])
+            
+        } else if(spaceArr[i] === '}' && 
+        spaceArr[i + 1] !== undefined && 
+        spaceArr[i + 1] !== '{' && 
+        spaceArr[i + 1] !== '}'){
+
+            inputArr.push(spaceArr[i])
+            inputArr.push(' ')
+        
+        //need to change this so that it works when the command ends and not when it finds a number (ie a variable after the command)
+        } else if (spaceArr[i] === '~' &&
+        slashStack.length !== 0 &&
+        bracketStack.length === 0){
+
+            slashStack.pop()
+            inputArr.push(' ')
+        
+        } else {
+            inputArr.push(spaceArr[i])
+        }
+    }
+
+    console.log('this one is inputarr')
+    console.log(inputArr);
+    inputArr = inputArr.join('').split(' ')    
+    
 
     // this for loop sepatates every character into a new array element unless it is a command
     for(let i = 0; i < inputArr.length; i++){
@@ -22,14 +75,15 @@ function translate(inputString){
         }
     }
 
-    let output = [];
+    console.log(inputQueue)
 
     for(let i = 0; i < inputQueue.length; i++){
         let char = inputQueue[i]
         
         //checks if the char is a number
         if (isNaN(Number(char)) !== true){ 
-            if(inputQueue[i - 1] !== undefined && inputQueue[i - 1].charAt(0) === '^'){
+            if(inputQueue[i - 1] !== undefined && 
+                inputQueue[i - 1].charAt(0) === '^'){
                 output.push('*');
                 output.push(inputQueue[i]);
 
@@ -42,8 +96,8 @@ function translate(inputString){
         
             switch(char){
                 //multiplication commands
-                case '\\cdot':
-                case '\\times':
+                case char.match(/^\\cdot/)?.input:
+                case char.match(/^\\times/)?.input:
                     output.push('*');
                     break;
 
@@ -63,7 +117,7 @@ function translate(inputString){
                 //sqrt (add toggle for fraction exponent instead of sqrt()?)
                 case char.match(/^\\sqrt/)?.input:
 
-                    let expression = translate(char.slice(5,-1));
+                    let expression = translate(char.slice(6,-1));
                     let sqrt = 'sqrt(' + expression + ')';
                     
                     output.push(sqrt);
@@ -91,7 +145,11 @@ function translate(inputString){
         else if (char.toUpperCase() != char.toLowerCase()){
 
             //if it is the first char in the string, no neet to add an asterix
-            if(inputQueue[i - 1] !== undefined){
+            if(inputQueue[i - 1] !== undefined &&
+                (accSym.indexOf(output[output.length - 1 ]) !== -1 &&
+                accSym.indexOf(output[output.length]) !== -1) ||
+                isNaN(Number(inputQueue[i - 1])) !== true){
+
                 output.push('*');
                 output.push(inputQueue[i]);
 
@@ -103,9 +161,7 @@ function translate(inputString){
         else if(accSym.indexOf(char) !== -1){
             output.push(inputQueue[i]);
         }
-
     }
-
     return output.join('');
 }
 
